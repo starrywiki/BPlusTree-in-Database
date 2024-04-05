3f669d591bc4a15306091289e924ed0cc19a6706
MelodicDrumstep <1724662527@qq.com>
2024-04-05 19:37:58 +0800

    first version

---
+++ src/include/storage/index/index_iterator.h
@@ -0,0 +1,57 @@
+//===----------------------------------------------------------------------===//
+//
+//                         DB Project 
+//                        
+//
+// Identification: src/include/index/index_iterator.h
+//
+// 
+//
+//===----------------------------------------------------------------------===//
+/**
+ * index_iterator.h
+ * For range scan of b+ tree
+ */
+#pragma once
+#include "storage/page/b_plus_tree_leaf_page.h"
+
+namespace bustub
+{
+
+#define INDEXITERATOR_TYPE IndexIterator<KeyType, ValueType, KeyComparator>
+
+INDEX_TEMPLATE_ARGUMENTS
+class IndexIterator
+{
+  public:
+  using LeafPage = BPlusTreeLeafPage<KeyType, ValueType, KeyComparator>;
+  // you may define your own constructor based on your member variables
+  IndexIterator(BufferPoolManager* buffer_pool_manager, page_id_t cur,
+                int index);
+  ~IndexIterator();  // NOLINT
+
+  auto IsEnd() -> bool;
+
+  auto operator*() -> const MappingType&;
+
+  auto operator++() -> IndexIterator&;
+
+  auto operator==(const IndexIterator& itr) const -> bool
+  {
+    return bpm_ == itr.bpm_ && cur_ == itr.cur_ && index_ == itr.index_;
+  }
+
+  auto operator!=(const IndexIterator& itr) const -> bool
+  {
+    return !(*this == itr);
+  }
+
+  private:
+  BufferPoolManager* bpm_;
+  page_id_t cur_;
+  int index_;
+  MappingType item_;
+  // add your own private member variables here
+};
+
+}  // namespace bustub
